e3d572718eb710c0f442332e9d91f36aeab95f33
LennyBu <118720768+LennyBu@users.noreply.github.com>
2023-02-08 13:14:18 +0100

    Delete N8_Web directory

--- N8_Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace N8_Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
